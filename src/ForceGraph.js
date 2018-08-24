f4dad743b2a9197348244f69b8cddb293ae51653
Chance Smith <chancesmithb@gmail.com>
2018-08-24 08:54:43 -0500

    📦  NEW: adds selectable nodes by box drawing

--- src/ForceGraph.js
+++ src/ForceGraph.js
@@ -5,6 +5,7 @@ let ctx;
 let network;
 let container;
 let canvas;
+let nodes;
 let rect = [];
 let drag = false;
 var drawingSurfaceImageData;
@@ -18,28 +19,31 @@ function restoreDrawingSurface() {
 }
 
 function selectNodesFromHighlight() {
-  // var fromX, toX, fromY, toY;
-  // var nodesIdInDrawing = [];
-  // var xRange = getStartToEnd(rect.startX, rect.w);
-  // var yRange = getStartToEnd(rect.startY, rect.h);
-  // var allNodes = nodes.get();
-  // for (var i = 0; i < allNodes.length; i++) {
-  //   var curNode = allNodes[i];
-  //   var nodePosition = network.getPositions([curNode.id]);
-  //   var nodeXY = network.canvasToDOM({
-  //     x: nodePosition[curNode.id].x,
-  //     y: nodePosition[curNode.id].y
-  //   });
-  //   if (
-  //     xRange.start <= nodeXY.x &&
-  //     nodeXY.x <= xRange.end &&
-  //     yRange.start <= nodeXY.y &&
-  //     nodeXY.y <= yRange.end
-  //   ) {
-  //     nodesIdInDrawing.push(curNode.id);
-  //   }
-  // }
-  // network.selectNodes(nodesIdInDrawing);
+  var fromX, toX, fromY, toY;
+  var nodesIdInDrawing = [];
+  var xRange = getStartToEnd(rect.startX, rect.w);
+  var yRange = getStartToEnd(rect.startY, rect.h);
+  var allNodes = nodes;
+  console.log("selectNodesFromHigh", xRange, yRange, allNodes, allNodes.length);
+  for (var i = 0; i < allNodes.length; i++) {
+    var curNode = allNodes[i];
+    var nodePosition = network.getPositions([curNode.id]);
+    var nodeXY = network.canvasToDOM({
+      x: nodePosition[curNode.id].x,
+      y: nodePosition[curNode.id].y
+    });
+    console.log("for each node", curNode, nodePosition, nodeXY);
+    if (
+      xRange.start <= nodeXY.x &&
+      nodeXY.x <= xRange.end &&
+      yRange.start <= nodeXY.y &&
+      nodeXY.y <= yRange.end
+    ) {
+      console.log("node added", curNode.id, nodesIdInDrawing);
+      nodesIdInDrawing.push(curNode.id);
+    }
+  }
+  network.selectNodes(nodesIdInDrawing);
 }
 
 function getStartToEnd(start, theLen) {
@@ -125,11 +129,12 @@ class ForceGraph extends Component {
 
   componentDidMount() {
     // ref on graph <Graph/> to add listeners to, maybe?
-    console.log("ref:", this.canvasWrapperRef.current.Network.canvas.frame);
+    console.log("ref:", this.canvasWrapperRef.current.Network.body.nodes);
 
     container = this.canvasWrapperRef.current.Network.canvas.frame;
     network = this.canvasWrapperRef.current.Network;
     canvas = this.canvasWrapperRef.current.Network.canvas.frame.canvas;
+    nodes = this.state.graph.nodes;
     ctx = canvas.getContext("2d");
 
     container.oncontextmenu = function() {
@@ -151,37 +156,39 @@ class ForceGraph extends Component {
           e.pageY -
           this.canvasWrapperRef.current.Network.body.container.offsetTop;
         this.state.drag = true;
-        // container[0].style.cursor = "default";
-        // selectNodesFromHighlight();
+        container.style.cursor = "default";
+        selectNodesFromHighlight();
       }
     });
 
     container.addEventListener("mousemove", e => {
-      restoreDrawingSurface();
-      rect.w =
-        e.pageX -
-        this.canvasWrapperRef.current.Network.body.container.offsetLeft -
-        rect.startX;
-      rect.h =
-        e.pageY -
-        this.canvasWrapperRef.current.Network.body.container.offsetTop -
-        rect.startY;
+      if (this.state.drag) {
+        restoreDrawingSurface();
+        rect.w =
+          e.pageX -
+          this.canvasWrapperRef.current.Network.body.container.offsetLeft -
+          rect.startX;
+        rect.h =
+          e.pageY -
+          this.canvasWrapperRef.current.Network.body.container.offsetTop -
+          rect.startY;
 
-      ctx.setLineDash([5]);
-      ctx.strokeStyle = "rgb(0, 102, 0)";
-      ctx.strokeRect(rect.startX, rect.startY, rect.w, rect.h);
-      ctx.setLineDash([]);
-      ctx.fillStyle = "rgba(0, 255, 0, 0.2)";
-      ctx.fillRect(rect.startX, rect.startY, rect.w, rect.h);
-      console.log("it's movin");
+        ctx.setLineDash([5]);
+        ctx.strokeStyle = "rgb(0, 102, 0)";
+        ctx.strokeRect(rect.startX, rect.startY, rect.w, rect.h);
+        ctx.setLineDash([]);
+        ctx.fillStyle = "rgba(0, 255, 0, 0.2)";
+        ctx.fillRect(rect.startX, rect.startY, rect.w, rect.h);
+        console.log("it's movin");
+      }
     });
 
     container.addEventListener("mouseup", e => {
       if (e.button == 2) {
-        // restoreDrawingSurface();
-        // this.state.drag = false;
-        // container[0].style.cursor = "default";
-        // selectNodesFromHighlight();
+        restoreDrawingSurface();
+        this.state.drag = false;
+        container.style.cursor = "default";
+        selectNodesFromHighlight();
         console.log("mouse up");
       }
     });
